60dfb57c7ab4952427c6816db22535afc4a667ef
Your Name <you@example.com>
2021-01-26 21:51:09 -0500

    Integracion de angular material

--- src/app/modules/shared/material.module.ts
+++ src/app/modules/shared/material.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
 
 /**
@@ -10,9 +7,6 @@ import { MatStepperModule } from '@angular/material/stepper';
  */
 @NgModule({
   exports: [
-    MatTableModule,
-    MatPaginatorModule,
-    MatDialogModule,
     MatStepperModule
   ]
 })
