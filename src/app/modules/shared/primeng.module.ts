515f8156c63d70c86a2976ee5e7c68518c101109
Your Name <you@example.com>
2021-01-26 21:45:17 -0500

    Se integra primeng al proyecto

--- src/app/modules/shared/primeng.module.ts
+++ src/app/modules/shared/primeng.module.ts
@@ -1,21 +1,5 @@
 import { NgModule } from '@angular/core';
-import { TreeTableModule } from 'primeng/treetable';
 import { TableModule } from 'primeng/table';
-import { CalendarModule } from 'primeng/calendar';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { PanelModule } from 'primeng/panel';
-import { CheckboxModule } from 'primeng/checkbox';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { KeyFilterModule } from 'primeng/keyfilter';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { TabViewModule } from 'primeng/tabview';
-import { ListboxModule } from 'primeng/listbox';
-import { FileUploadModule } from 'primeng/fileupload';
-import {AutoCompleteModule} from 'primeng/autocomplete';
 
 /**
  * Modulo que contiene los modulos especificos
@@ -23,42 +7,10 @@ import {AutoCompleteModule} from 'primeng/autocomplete';
  */
 @NgModule({
   imports: [
-    TreeTableModule,
-    TableModule,
-    CalendarModule,
-    DialogModule,
-    DropdownModule,
-    PanelModule,
-    CheckboxModule,
-    RadioButtonModule,
-    KeyFilterModule,
-    InputSwitchModule,
-    TriStateCheckboxModule,
-    OverlayPanelModule,
-    MultiSelectModule,
-    TabViewModule,
-    ListboxModule,
-    FileUploadModule,
-    AutoCompleteModule
+    TableModule
   ],
   exports: [
-    TreeTableModule,
-    TableModule,
-    CalendarModule,
-    DialogModule,
-    DropdownModule,
-    PanelModule,
-    CheckboxModule,
-    RadioButtonModule,
-    KeyFilterModule,
-    InputSwitchModule,
-    TriStateCheckboxModule,
-    OverlayPanelModule,
-    MultiSelectModule,
-    TabViewModule,
-    ListboxModule,
-    FileUploadModule,
-    AutoCompleteModule
+    TableModule
   ]
 })
 export class PrimengModule { }
